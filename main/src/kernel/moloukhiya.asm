9d601b03c8ce2099e8579c1c3994fc763ef1d385
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2020-12-25 20:39:46 +0300

    Update moloukhiya.asm

--- main/src/kernel/moloukhiya.asm
+++ main/src/kernel/moloukhiya.asm
@@ -1,8 +1,19 @@
-%define defint(interrupt, handler) mov [interrupt*4], dword handler
+%include "src/shared/keyboard.asm"
 [org 0x7e00]
 [bits 16]
 section .text
 
-jmp $
-times 510-($-$$) db 0
-dw 0xaa55
+call CreateHUD
+
+loop:
+
+call UpdateHUD
+
+jmp loop
+
+lshiftdown: db 'LShift down',0x0D,0x0A,0
+rshiftdown: db 'RShift down',0x0D,0x0A,0
+
+%include "src/kernel/hud.asm"
+
+times 0x8000-($-$$) db 0;
